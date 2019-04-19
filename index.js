ad39415e4b7d82fb3f25678344a8fe3490cb2027
zee724 <46341377@qq.com>
2019-04-19 23:06:50 +0800

    Change Port to 80

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.set('port',(process.env.PORT || 5000));
+app.set('port',(process.env.PORT || 80));
 app.get('/',(req,res) => {
   res.send('Hello | ' + new Date(Date.now()));
 });
